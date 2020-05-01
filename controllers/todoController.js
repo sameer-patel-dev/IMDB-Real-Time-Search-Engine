6191c4b115ce7db3fb538f76d4dae635fece8a9a
Sameer <sameer.patel@somaiya.edu>
2020-05-01 20:32:47 +0530

    Added MongoDb

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,26 +1,47 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
+mongoose.connect('mongodb+srv://test:test@todo-wpmpy.mongodb.net/test?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology:true})
 
-var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+
+// var itemOne = Todo({item: 'buy flowers'}).save(function(err){
+//     if (err) throw err;
+//     console.log('Item Saved');
+// });
+
+// var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];
 
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 module.exports = function(app){
     
     app.get('/todo', function(req, res){
-        res.render('todo', {todos: data});
+        //get data from mongodb and pass it to the view
+        Todo.find({}, function(err, data){
+            if (err) throw err;
+            res.render('todo', {todos: data});
+        });
     });
 
     app.post('/todo', urlencodedParser, function(req, res){
-        data.push(req.body);
-        res.json(data); 
+        //get data from the view and add it to the mongodb
+        var newTodo = Todo(req.body).save(function(err,data){
+            if (err) throw err;
+            res.json(data);
+        });
     });
 
     app.delete('/todo/:item', function(req, res){
-        data = data.filter(function(todo){
-            return todo.item.replace(/ /g, "-") !== req.params.item;
+        //delete requested item from mongodb
+        Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function(err,data){
+            if (err) throw err;
+            res.json(data);
         });
-        res.json(data);
     });
 
 };
